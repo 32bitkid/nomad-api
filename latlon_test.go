418524eb3f3b04fc92f8fb3f0084819e85f51804
James Holmes <32bitkid@gmail.com>
2013-10-05 22:48:53 +0000

    Removing artifically broken test. :(

--- latlon_test.go
+++ latlon_test.go
@@ -7,21 +7,14 @@ import (
 
 func TestSomeGarbage(t *testing.T) {
 	var err error
-
-	file, err := os.Open("test-data/CTR2013.gpx")
-	if err != nil {
-		t.Error(err)
-	}
+  
+  file, err := os.Open("test-data/CTR2013.gpx")
+  if err != nil { t.Error(err) }
 	g, err := FromXml(file)
-	if err != nil {
-		t.Error(err)
-	}
+  if err != nil { t.Error(err) }
 
-	l := len(g.TrackPoints())
+  l := len(g.TrackPoints())
 	if l != 10000 {
 		t.Errorf("Did not unmarshal the test data correctly. %d != 10000", l)
 	}
-
-	t.Log(g.TrackPoints()[0])
-	t.Fail()
 }
